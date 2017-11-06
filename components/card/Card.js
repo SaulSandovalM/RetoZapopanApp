3090457ffa58aaaac0b419b0d473f108529d3cb2
JoseLAmador <josel.amador.s@gmail.com>
2017-11-06 13:32:12 -0600

    Cardd

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,107 +1,78 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, ScrollView, Image} from 'react-native';
-import {Container, Card, CardItem, Button, Icon, Left, Body, Right} from 'native-base';
-import Cabecera from './components/detalle/Cabecera';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+import * as Progress from 'react-native-progress';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Icon, Left, Body, Right } from 'native-base';
 
-export default class Card extends Component < {} > {
-  render() {
-    return (
-      <Container>
-      <Cabecera/>
-      <ScrollView>
-        <Card style={styles.card}>
-          <CardItem>
-            <Body>
+class Cardd extends Component {
+  render(){
+    return(
+      <TouchableOpacity>
+      <Card style={{flex:0, width:'95%', alignSelf:'center', marginTop:10}}>
+
+        <CardItem style={{justifyContent:'center', alignItems:'center'}}>
+          <Text style={{fontSize: 20, fontWeight:'bold'}}>Plataforma Crowfundig</Text>
+          </CardItem>
+
+          <CardItem cardBody>
+
               <Image source={{
-                  uri: 'https://www.tensorflow.org/_static/images/tensorflow/logo.png'
-                }} style={styles.image}/>
-              <Text style={styles.title}>
-                TensorFlow
-              </Text>
-            </Body>
-          </CardItem>
+                uri: 'https://i.ytimg.com/vi/nZjiF0KTR-E/maxresdefault.jpg'}}
+                style={{height: 300, width: '100%',
+                flex: 1,
+                justifyContent:'space-between'
+                }}
+              >
 
-          <CardItem>
-            <Body>
-              <Text style={styles.text}>Saving lifes is a program that try to compare the prices of drugs so you
-                can choose the best option</Text>
-            </Body>
-          </CardItem>
+                  <View style={{
+                      height: 60,
+                      width: null,
+                      alignItems:'center',
+                      flexDirection:'row',
+                      justifyContent:'flex-end',
+                      padding: 5
+                  }}
+                  >
+                      <View style={{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                          <Icon name="ios-bookmarks" style={{color:'white', alignSelf:'center'}}/>
+                          <Text style={{color:'white',}}>Tecnología e Innovación</Text>
+                      </View>
 
-          <CardItem>
-            <Left>
-              <Image source={{
-                  uri: 'https://avatars2.githubusercontent.com/u/20559576?s=460&v=4'
-                }} style={styles.imgPerfil}/>
-              <Body>
-                <Text>Saul Sandoval M</Text>
-              </Body>
-            </Left>
-          </CardItem>
+                  </View>
 
-          <CardItem style={styles.carditem}>
-            <Left>
-              <Button transparent textStyle={{
-                  color: '#87838B'
-                }}>
-                <Text>
-                  14 Horas {'\n'}Restantes</Text>
-              </Button>
-            </Left>
+              <View style={{
+                  backgroundColor: 'rgba(0,0,0,.7)',
+                  height: 60,
+                  width: null,
+                  alignItems:'center',
+                  flexDirection:'row',
+                  justifyContent:'space-between',
+                  padding: 5
+                  }}
+              >
+                  <View>
+                      <Progress.Pie progress={0.2} size={25} color={'white'} style={{alignSelf:'center'}}/>
+                      <Text style={{color:'white'}}> Recaudado</Text>
+                  </View>
 
-            <Body>
-              <Button transparent textStyle={{
-                  color: '#87838B'
-                }}>
-                <Text>
-                  14 mil {'\n'}Donadores</Text>
-              </Button>
-            </Body>
+                  <View>
+                      <Icon name="person" style={{color:'white', alignSelf:'center'}}/>
+                      <Text style={{color:'white'}}>100 Donadores</Text>
+                  </View>
 
-            <Right>
-              <Button transparent textStyle={{
-                  color: '#87838B'
-                }}>
-                <Text>
-                  $ 10,000 {'\n'}Recaudado</Text>
-              </Button>
-            </Right>
-          </CardItem>
+                  <View>
+                      <Icon name="ios-clock-outline" style={{color:'white', alignSelf:'center'}}/>
+                      <Text style={{color:'white'}}>14 Hrs restantes</Text>
+                  </View>
 
-        </Card>
-      </ScrollView>
-    </Container>
+
+              </View>
+              </Image>
+          </CardItem >
+
+          </Card>
+          </TouchableOpacity>
     );
-  }
-}
+  };
+};
 
-const styles = StyleSheet.create({
-  card: {
-    flex: 0
-  },
-  image: {
-    height: 200,
-    width: '100%',
-    flex: 1
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 20
-  },
-  text: {
-    fontSize: 16,
-    margin: 20,
-    marginTop: -20
-  },
-  imgPerfil: {
-    height: 30,
-    width: 30,
-    borderRadius: 15,
-    marginLeft: 20
-  },
-  carditem: {
-    marginLeft: 20,
-    marginRight: 20
-  }
-});
+export default Cardd;
